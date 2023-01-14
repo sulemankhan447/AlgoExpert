9244286660341dcc62e1ba2c74a704420972dccc
Suleman Khan <sulemankhan447@gmail.com>
2023-01-14 22:11:32 +0530

    Implemented palindrome check using list

--- app/src/main/java/com/problemsolving/other/CheckPalindrome.kt
+++ app/src/main/java/com/problemsolving/other/CheckPalindrome.kt
@@ -2,13 +2,30 @@ package com.problemsolving.other
 
 fun main() {
 
-//    println(checkPalindrome("suleman"))
-//    println(checkPalindrome("mom"))
-    println(checkPalindromeUsingMap("ninjit"))
+//    println(checkPalindromeUsingList("suleman"))
+    println(checkPalindromeUsingList("mom"))
+//    println(checkPalindromeUsingList("niint"))
 
 }
 
+fun checkPalindromeUsingList(data: String): Boolean {
+    //Time - O(n), space - O(n)
+    var list = mutableListOf<Char>()
+    for (i in 0 until data.length) {
+        if (list.contains(data[i]))
+            list.remove(data[i])
+        else
+            list.add(data[i])
+    }
+    return if (data.length % 2 == 0) {
+        list.isEmpty()
+    } else {
+        list.size == 1
+    }
+}
+
 fun checkPalindromeUsingMap(data: String): Boolean {
+    //Time - O(n), space - O(n)
     var oddCount = 0
     val hashMap = mutableMapOf<Char, Int>()
     for (i in 0 until data.length) {
@@ -28,3 +45,10 @@ fun checkPalindromeUsingMap(data: String): Boolean {
     }
     return true
 }
+
+/**
+ * https://www.geeksforgeeks.org/check-characters-given-string-can-rearranged-form-palindrome/
+ * to solve problem in
+ * o(n) time and o(1) space
+ * refer this link for bits implementation
+ */
